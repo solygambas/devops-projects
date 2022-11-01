71d0bfae64891e673ccfa5dd954baf47c6c2702e
solygambas <51904909+solygambas@users.noreply.github.com>
2022-11-01 18:31:42 +0100

    change h2 to h3

--- playground/01-react-test/src/components/HelpBox.jsx
+++ playground/01-react-test/src/components/HelpBox.jsx
@@ -1,11 +1,11 @@
-import { string } from 'prop-types';
+import { string } from "prop-types";
 
-import './HelpBox.css';
+import "./HelpBox.css";
 
 function HelpBox({ title, text }) {
   return (
     <article className="help-box">
-      <h2>{title}</h2>
+      <h3>{title}</h3>
       <p>{text}</p>
     </article>
   );
