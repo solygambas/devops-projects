3cef5a8389465488058e7ad02b97efe1850cdfc3
solygambas <51904909+solygambas@users.noreply.github.com>
2022-11-02 11:03:26 +0100

    add vite config

--- playground/01-react-test/vite.config.js
+++ playground/01-react-test/vite.config.js
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import eslint from 'vite-plugin-eslint';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint()],
+  plugins: [react()],
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/test/setup.js',
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.js",
+    reporters: ["json"],
+    outputFile: "test.json",
   },
 });
