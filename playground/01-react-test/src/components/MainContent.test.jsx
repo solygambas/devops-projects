4ea054b2fae0c48c3c08c72ae8731482394de66e
solygambas <51904909+solygambas@users.noreply.github.com>
2022-11-02 11:24:13 +0100

    fix test

--- playground/01-react-test/src/components/MainContent.test.jsx
+++ playground/01-react-test/src/components/MainContent.test.jsx
@@ -16,6 +16,6 @@ describe("MainContent", () => {
 
     const button = screen.getByRole("button");
     await userEvent.click(button);
-    expect(screen.getByTestId("help-are")).toBeInTheDocument();
+    expect(screen.getByTestId("help-area")).toBeInTheDocument();
   });
 });
